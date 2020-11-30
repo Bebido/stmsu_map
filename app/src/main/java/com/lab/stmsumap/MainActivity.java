63e70e74563bd08c9735a463a5ddb32edebb04c4
mpucilowski <mariusz.pucilowski@atos.net>
2020-11-30 11:02:01 +0100

    Created app layout

--- app/src/main/java/com/lab/stmsumap/MainActivity.java
+++ app/src/main/java/com/lab/stmsumap/MainActivity.java
@@ -1,13 +1,35 @@
 package com.lab.stmsumap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button buttonOriginalMap;
+    Button buttonMapSnippet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setFields();
+    }
+
+    private void setFields() {
+        imageView = findViewById(R.id.imageView);
+        buttonOriginalMap = findViewById(R.id.buttonOriginalMap);
+        buttonMapSnippet = findViewById(R.id.buttonMapSnippet);
+    }
+
+    public void showOriginalMap(View view) {
+        //do nothing for now
+    }
+
+    public void showMapSnippet(View view) {
+        //do nothing for now
     }
 }
